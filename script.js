fc746dbad0d854f34f2d69d48bfb5d83681ae5f1
nikusha <nikushadzamsashvili@gmail.com>
2025-12-03 09:37:23 +0400

    feat
    დამატებულია ცვლადების javascript ამოცანა

---
+++ script.js
@@ -0,0 +1,19 @@
+const firstName = "ნიკა";
+
+const lastName = "ძამსაშვილი";
+
+const email = "nikadzamsashvili@gmail.com";
+
+const phoneNumber = "+995 599 585 588";
+
+let age = 28;
+
+const isAdult = age >= 18;
+
+console.log("სახელი", firstName);
+console.log("გვარი", lastName);
+console.log("ასაკი", age);
+console.log("იმეილი", email);
+console.error("ტელ.ნომერი", phoneNumber);
+console.warn("არის თუ არა სრრულწლოვანი", isAdult);
+console.log("ასაკის შეცვლის შემდეგ:", age);
